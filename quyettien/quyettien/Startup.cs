fd2f4db768e784762bf41dfea624ce10726b7938
US <phanphuocloc135@gmail.com>
2018-11-21 10:46:46 +0700

    quyettien

---
+++ quyettien/quyettien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(quyettien.Startup))]
+namespace quyettien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
